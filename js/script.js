3fe7139d2744dc19bc6618b165c128af4b26d703
Sonia Skowron-Zarebska <ssoniq7@gmail.com>
2023-05-30 22:42:00 +0100

    removeTask and toggleTaskDone functions changed

--- js/script.js
+++ js/script.js
@@ -13,17 +13,14 @@
     };
 
 
-  const removeTask = (taskIndex) => {
-    tasks = [
-      ...tasks.slice(0, taskIndex),
-      ...tasks.slice(taskIndex + 1),
-    ];
-
-    render();
-  };
+    const removeTask = (taskIndex) => {
+      tasks = tasks.filter((task, index) => index !== taskIndex);
+  
+      render();
+    };
 
   const toggleTaskDone = (taskIndex) => {
-    tasks[taskIndex].done = !tasks[taskIndex].done;
+    tasks = tasks.map((task, index) => index === taskIndex ? { ...task, done: !task.done } : task);
 
     render();
   };
